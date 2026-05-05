aefbe54dd2493cba65c0f6eb08fafb46158a9a76
Ángel Cruz-DS <joredwg@gmail.com>
2026-05-05 17:11:32 -0600

    funciones para Convertir texto, truncar y limpia

--- reto-semana-01/main.py
+++ reto-semana-01/main.py
@@ -16,10 +16,43 @@ def lim_val(valor):
             resultado += char
     return resultado
 
+def conv_ent(texto):
+    """"
+    Convierte texto a entero turncando decimales
+    para textos vacios o invalidos da un 0
+    """
+    #Para cadenas vacias
+    if not texto:
+        return 0
+    
+    #red de seguridad
+    try:
+        numero = float(texto) #Convierte a numwro con decimales
+        return int(numero) #trunca
+    except ValueError:
+        return 0 #0 en caso de error
+    
+
+
+
+
+
 def procc_lin(linea):
+    """
+    -quita los espacios en blanco de los extremos
+    -Separa por comas
+    -Limpia, trunca y suma cada valor
+    """
     pass
+
+
+
+
+
+
+
+
 def main ():
     pass
 if __name__ == "__main__":
     main()
-    
